f1e35a66cd2fdff36a224dffaa9cf31987de6fae
hapo3005 <jan.nikolic@web.de>
2026-09-14 17:09:12 +0200

    Refresh blocked period date range module

--- admin-calendar-views.js
+++ admin-calendar-views.js
@@ -20,4 +20,4 @@
   Object.assign(A,{initCalendarViews,renderCalendarAdvanced});
 })();
 import('./admin-customer-detail.js');
-import('./admin-recurring-blocks.js?v=20260914-1436').then(()=>window.SSAdmin?.initRecurringBlocks?.());
+import('./admin-recurring-blocks.js?v=20260914-1503').then(()=>window.SSAdmin?.initRecurringBlocks?.());
