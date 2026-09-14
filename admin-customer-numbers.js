573079e24ce48694250644d9f78f7b44416f5d31
hapo3005 <jan.nikolic@web.de>
2026-09-14 16:56:29 +0200

    Load realistic demo customer profiles

--- admin-customer-numbers.js
+++ admin-customer-numbers.js
@@ -89,3 +89,4 @@
 
   Object.assign(A,{initCustomerNumbers,ensureCustomerNumbers,nextCustomerNumber,formatCustomerNumber,renderCustomersWithNumbers});
 })();
+import('./admin-demo-profiles.js?v=20260914-1455').then(()=>window.SSAdmin?.initDemoProfiles?.());
