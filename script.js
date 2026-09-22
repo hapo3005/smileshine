4033a4449694b6bcf39b4aa218adc78eb9b9c061
hapo3005 <jan.nikolic@web.de>
2026-09-22 13:47:24 +0200

    Unify public modules on frontend pass 2

--- script.js
+++ script.js
@@ -202,6 +202,6 @@ if(waitlistToggle&&waitlistForm){
 
 window.SmileShineBooking={state:bookingState,updateSummary,buildDates,buildTimes,setStep,selectServiceButton};
 updateSummary();
-import('./checkout-enhancements.js?v=20260922-public-cleanup1');
-import('./booking-admin-sync.js?v=20260922-public-cleanup1');
-import('./cnc-products-carousel.js?v=20260922-frontend-pass1');
+import('./checkout-enhancements.js?v=20260922-frontend-pass2');
+import('./booking-admin-sync.js?v=20260922-frontend-pass2');
+import('./cnc-products-carousel.js?v=20260922-frontend-pass2');
