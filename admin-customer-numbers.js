802e802f0fdc34dc6cd453b3c887452638fcc28b
hapo3005 <jan.nikolic@web.de>
2026-09-23 19:08:41 +0200

    Refresh Birgit customer module cache

--- admin-customer-numbers.js
+++ admin-customer-numbers.js
@@ -172,5 +172,5 @@
 
   Object.assign(A,{initCustomerNumbers,ensureCustomerNumbers,nextCustomerNumber,formatCustomerNumber,renderCustomersWithNumbers,renderCustomerWorkspaceSummary,applyCustomerFilter});
 })();
-import('./admin-demo-profiles.js?v=20260923-birgit-polish2').then(()=>window.SSAdmin?.initDemoProfiles?.());
-import('./admin-whatsapp.js?v=20260923-birgit-polish2').then(()=>window.SSAdmin?.initWhatsApp?.());
+import('./admin-demo-profiles.js?v=20260923-birgit-final2').then(()=>window.SSAdmin?.initDemoProfiles?.());
+import('./admin-whatsapp.js?v=20260923-birgit-final2').then(()=>window.SSAdmin?.initWhatsApp?.());
